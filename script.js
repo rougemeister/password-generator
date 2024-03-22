7b033aa59560ba5239683ef274e1024fdedabb1c
kruzznitsch <rougemeister1@gmail.com>
2024-03-22 04:15:41 +0000

    Removed default parameters

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+"O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g",
+"h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%",
+"^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",
+".","?",
+"/"];
 
 const generateEl = document.querySelector('#generate-el');
 const passwordOne = document.querySelector('.password-one');
@@ -7,14 +14,8 @@ const passwordTwo = document.querySelector(".password-two")
 
 
 const generatePassword = (
-    length = 7,
-    characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
-    "O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b",
-    "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
-    "r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", 
-    "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")",
-    "_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-    "/"]
+    length,
+    characters
   ) =>
     Array.from(crypto.getRandomValues(new Uint32Array(length)))
       .map((x) => characters[x % characters.length])
@@ -23,8 +24,8 @@ const generatePassword = (
 
 
 function renderPassword (){
-    let randomStringOne = generatePassword();
-    let randomStringTwo = generatePassword();
+    let randomStringOne = generatePassword(15, characters);
+    let randomStringTwo = generatePassword(15, characters);
     passwordOne.textContent = randomStringOne;
     passwordTwo.textContent = randomStringTwo;
 }
